a9041209320ec93789d5556b3d51da182117254a
孝史 <@192.168.145.1>
2019-02-16 22:50:18 +0900

    【#32】spring-bootのApplicationRunnerインターフェースを用いるよう修正

--- src/main/java/jp/co/sunarch/sakatube/SakaTubeApplication.java
+++ src/main/java/jp/co/sunarch/sakatube/SakaTubeApplication.java
@@ -1,10 +1,12 @@
 package jp.co.sunarch.sakatube;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SakaTubeApplication {
+public class SakaTubeApplication implements ApplicationRunner {
 
 	private static GenerateDB generateDb;
 
@@ -14,6 +16,11 @@ public class SakaTubeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SakaTubeApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments arg0) throws Exception {
+		// DB環境構築
 		generateDb.createDB();
 	}
 }
